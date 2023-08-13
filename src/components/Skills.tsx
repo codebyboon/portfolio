b07829b0b0d1bcdca503942f685584e21fa2a023
codebyboon <ah_boon95@hotmail.com>
2023-08-14 02:23:10 +0800

    Updated icons path

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,6 +3,18 @@
 import React from 'react'
 import Image from 'next/image'
 
+import sqlIcon from '/public/images/sql-icon.png';
+import tailwindIcon from '/public/images/tailwind-icon.png';
+import pythonIcon from '/public/images/python-icon.png';
+import jsIcon from '/public/images/js-icon.png';
+import nextIcon from '/public/images/nextjs-icon.png';
+import reactIcon from '/public/images/react-icon.png';
+import informixIcon from '/public/images/informix-icon.png';
+import oracleIcon from '/public/images/oracle-siebel.svg'
+import tsIcon from '/public/images/typescript-icon.png'
+import goIcon from '/public/images/golang-icon.png'
+
+
 /**
  * * Design of Rating bar for each skills
  */
@@ -26,7 +38,7 @@ const Ratingbar = ({skill, style, progress, imgsrc} : {skill: string, style: str
 const Skills = () => {
 
   return (
-    <div className='my-10 scroll-mt-28' id='skills'>
+    <div className='my-auto scroll-mt-28' id='skills'>
       <h2 className='font-bold md:text-6xl sm:text-5xl xs:text-4xl text-3xl mt-10 lg:mt-0 mb-10 lg:mb-20 w-full text-center text-[#fffffe]'>
         Skills
       </h2>
@@ -35,34 +47,34 @@ const Skills = () => {
         <div>
           <h3 className='capitalize mb-5'>Proficient Tech/Languages&nbsp;</h3>
           <div className='w-auto min-[400px]:w-96'>
-            <Ratingbar skill="Siebel" style="skills siebel" progress="90%" imgsrc="/images/oracle-siebel.svg" />
-            <Ratingbar skill="SQL" style="skills sql" progress="90%" imgsrc="/images/sql-icon.png"  />
-            <Ratingbar skill="Javascript" style="skills js" progress="80%" imgsrc="/images/js-icon.png" />
-            <Ratingbar skill="Informix" style="skills informix" progress="80%" imgsrc="/images/informix-icon.png" />
-            <Ratingbar skill="Python" style="skills python" progress="75%" imgsrc="/images/python-icon.png" />
+            <Ratingbar skill="Siebel" style="skills siebel" progress="90%" imgsrc={oracleIcon} />
+            <Ratingbar skill="SQL" style="skills sql" progress="90%" imgsrc={sqlIcon}  />
+            <Ratingbar skill="Javascript" style="skills js" progress="80%" imgsrc={jsIcon}/>
+            <Ratingbar skill="Informix" style="skills informix" progress="80%" imgsrc={informixIcon}/>
+            <Ratingbar skill="Python" style="skills python" progress="75%" imgsrc={pythonIcon} />
           </div>
         </div>
             
         <div>
           <h3 className='capitalize mb-1 mt-10 lg:mt-0 lg:mb-5'>Experience&nbsp;</h3>
           <div className='w-auto min-[400px]:w-96'>
-            <Ratingbar skill="Tailwind CSS" style="skills tailwind" progress="70%" imgsrc="/images/tailwind-icon.png"/>
-            <Ratingbar skill="Typescript" style="skills ts" progress="70%" imgsrc="/images/typescript-icon.png" />
-            <Ratingbar skill="React" style="skills react" progress="65%" imgsrc="/images/react-icon.png" />
-            <Ratingbar skill="NextJS" style="skills next" progress="60%" imgsrc="/images/nextjs-icon.png" />
-            <Ratingbar skill="Go" style="skills go" progress="50%" imgsrc="/images/golang-icon.png" />
+            <Ratingbar skill="Tailwind CSS" style="skills tailwind" progress="70%" imgsrc={tailwindIcon}/>
+            <Ratingbar skill="Typescript" style="skills ts" progress="70%" imgsrc={tsIcon}/>
+            <Ratingbar skill="React" style="skills react" progress="65%" imgsrc={reactIcon}/>
+            <Ratingbar skill="NextJS" style="skills next" progress="60%" imgsrc={nextIcon}/>
+            <Ratingbar skill="Go" style="skills go" progress="50%" imgsrc={goIcon}/>
           </div>
         </div>
 
         <div>
           <h3 className='capitalize mb-1 mt-10 lg:mt-0 lg:mb-5'>Other skills&nbsp;</h3>
           <div className='w-auto min-[400px]:w-96'>
-            <Ratingbar skill="Requirements Gathering & Analysis" style="skills requirements" progress="90%" imgsrc="/images/requirements-icon.png"/>
-            <Ratingbar skill="Waterfall Methodology" style="skills waterfall" progress="90%" imgsrc="/images/waterfall-icon.png"/>
-            <Ratingbar skill="Business Analysis" style="skills ba" progress="80%" imgsrc="/images/ba-icon.png"/>
-            <Ratingbar skill="Agile Methodology" style="skills agile" progress="70%" imgsrc="/images/agile-icon.png"/>
-            <Ratingbar skill="Project Management" style="skills project" progress="70%" imgsrc="/images/pm-icon.png"/>
-            <Ratingbar skill="Team Management" style="skills team" progress="60%" imgsrc="/images/team-icon.png"/>
+            <Ratingbar skill="Requirements Gathering & Analysis" style="skills requirements" progress="90%" imgsrc="/../public/images/requirements-icon.png"/>
+            <Ratingbar skill="Waterfall Methodology" style="skills waterfall" progress="90%" imgsrc="/../public/images/waterfall-icon.png"/>
+            <Ratingbar skill="Business Analysis" style="skills ba" progress="80%" imgsrc="/../public/images/ba-icon.png"/>
+            <Ratingbar skill="Agile Methodology" style="skills agile" progress="70%" imgsrc="/../public/images/agile-icon.png"/>
+            <Ratingbar skill="Project Management" style="skills project" progress="70%" imgsrc="/../public/images/pm-icon.png"/>
+            <Ratingbar skill="Team Management" style="skills team" progress="60%" imgsrc="/../public/images/team-icon.png"/>
           </div>
         </div>
       </div>
